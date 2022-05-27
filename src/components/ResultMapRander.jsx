b1ad6d5c476ae3e9d54006366597fac5c41e4629
Jiwon8037 <lego2090@naver.com>
2022-05-27 22:37:12 +0900

    날짜별 지도출력, 인포위도우 제어기능 추가

--- src/components/ResultMapRander.jsx
+++ src/components/ResultMapRander.jsx
@@ -13,15 +13,14 @@ const ResultMapRander = ({placeData}) => {
         // 지도를 생성합니다    
         var map = new kakao.maps.Map(mapContainer, mapOption);
         var linePath=[];
-        var wtmCoords=[];
         var bounds=new kakao.maps.LatLngBounds();// 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
 
         var marker,i;
         for(i=0; i<mapRandersPlaceData.length; i++){
             const {place_name, x, y}=mapRandersPlaceData[i];
-            const coordinate=new kakao.maps.LatLng(y,x);
-            const wtm= coordinate.toCoords();
-            var temp;
+            const WGS84=new kakao.maps.LatLng(y,x);
+            const WTM= WGS84.toCoords();
+            var temp;//장소이름 임시저장
 
             if(mapRandersPlaceData[i-1]===undefined){
                 temp='';
@@ -29,30 +28,31 @@ const ResultMapRander = ({placeData}) => {
                 temp=mapRandersPlaceData[i-1].place_name;
             };
 
-            linePath.push(coordinate);
-            wtmCoords.push(wtm);
+            linePath.push(WGS84);
 
             marker = new kakao.maps.Marker({
                 map: map, // 마커를 표시할 지도
-                position: coordinate, // 마커를 표시할 위치
+                position: WGS84, // 마커를 표시할 위치
                 title : place_name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다 
+                clickable:true
             });
             marker.setMap(map);
-            
             bounds.extend(linePath[i]);// LatLngBounds 객체에 좌표를 추가합니다
 
             // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-            var iwContent = `<div style="padding:5px;">${i+1} : ${place_name}<br><a href="https://map.kakao.com/?map_type=TYPE_MAP&target=car&rt=${',,'+wtmCoords[i].La+','+wtmCoords[i].Ma}&rt1=${temp}&rt2=${place_name}&rtIds=%2C&rtTypes=%2C" style="color:blue" target="_blank">길찾기</a></div>`,
-                iwPosition = coordinate; //인포윈도우 표시 위치입니다
-            
+            var iwContent = `<div style="padding:8px;white-space:nowrap;">${i+1} : ${place_name}<br><a href="https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt=${',,'+WTM.La+','+WTM.Ma}&rt1=${temp}&rt2=${place_name}&rtIds=%2C&rtTypes=%2C" style="color:blue" target="_blank">길찾기</a></div>`,
+                iwPosition = WGS84, //인포윈도우 표시 위치입니다
+                iwRemoveable = true;
+
             var infowindow = new kakao.maps.InfoWindow({
                 position : iwPosition, 
-                content : iwContent 
+                content : iwContent,
+                removable: iwRemoveable
             });
-            // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-            infowindow.open(map, marker);
+            
+            kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow));
         };
-        
+
         var polyline = new kakao.maps.Polyline({
             path: linePath, // 선을 구성하는 좌표배열 입니다
             strokeWeight: 5, // 선의 두께 입니다
@@ -68,6 +68,14 @@ const ResultMapRander = ({placeData}) => {
             map.setBounds(bounds);
         };
         setBounds();
+
+        // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+        function makeOverListener(map, marker, infowindow) {
+            return function() {
+                infowindow.open(map, marker);
+            };
+        };
+
     },[placeData]);
 
     return (
