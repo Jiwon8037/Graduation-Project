0febc9d114643a97a500136a4c255c0b9bea11ea
Jiwon8037 <lego2090@naver.com>
2022-06-10 15:49:11 +0900

    footer 위치 수정

--- src/pages/FooterTemplate.jsx
+++ src/pages/FooterTemplate.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter=styled.div`
+    background-color: white;
     border-top: 2px solid #ccc;
-    border-bottom: 1px solid #ccc;
+    //border-bottom: 1px solid #ccc;
     .footerDiv{
-        margin: 0 2%;
+        margin: 1% 2%;
         display: inline-flex;
     }
     h5{
