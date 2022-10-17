528e2953ef001ec53ed7769b0bdf34ac5285ed2f
Jiwon8037 <lego2090@naver.com>
2022-10-17 12:21:51 +0900

    푸터 수정

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -9,10 +9,16 @@ const Footer = () => {
       <div className="footerDiv">
         <img className="sku" alt="sku" src={imgSku} />
         <h5>경기도 안양시 만안구 성결대학로 성결관 101호 물리실습실</h5>
-        <h5>2조 Horizon 강지원</h5>
-        <a href="https://github.com/Jiwon8037">
+        <h5>2조 Horizon 강지원 박수영 윤성현 이태욱 박소은</h5>
+        <h5>FRONT END</h5>
+        <a href="https://github.com/Jiwon8037/Graduation-Project">
           <img className="git" alt="github" src={imgGit} />
         </a>
+        <h5>BACK END</h5>
+        <a href="https://github.com/FouinK/Graduation_project-SpringBoot-">
+          <img className="git" alt="github" src={imgGit} />
+        </a>
+        <br/>
       </div>
     </FooterTemplate>
   );
